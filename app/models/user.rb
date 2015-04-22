ff215e3552e0c238a0cdb8c7d116d5f7883030e7
toyama satoshi <toyamarinyon@gmail.com>
2015-04-23 08:50:34 +0900

    Fix user remember

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
-    update_attribute :remember_token, User.digest(remember_token)
+    update_attribute :remember_digest, User.digest(remember_token)
   end
 
   # Returns true if the given token matches the digest.
