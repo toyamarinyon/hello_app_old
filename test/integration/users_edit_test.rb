e78e901b61cd57beaa09a7bbd7dc5ffdaa7c6177
toyama satoshi <toyamarinyon@gmail.com>
2015-04-23 10:41:34 +0900

    Fix test fecture user name

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,10 +3,11 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users :micheal
+    @user = users :michael
   end
 
   test "unsuccessful edit" do
+    log_in_as @user
     get edit_user_path @user
     assert_template 'users/edit'
     patch user_path @user, user: { name: "",
@@ -17,6 +18,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as @user
     get edit_user_path @user
     assert_template 'users/edit'
     name = "Foo Bar"
