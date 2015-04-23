8f60cdbe40dd70e0e9fca2a5acbf9ce24f03077d
toyama satoshi <toyamarinyon@gmail.com>
2015-04-23 09:58:32 +0900

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421030858) do
+ActiveRecord::Schema.define(version: 20150422094958) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150421030858) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
