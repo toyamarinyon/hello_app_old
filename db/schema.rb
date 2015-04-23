f0d254a220c1af56128c6383943b2b0f4cbc17ed
toyama satoshi <toyamarinyon@gmail.com>
2015-04-23 20:55:41 +0900

    Chapter 10.2.3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423102912) do
+ActiveRecord::Schema.define(version: 20150423114014) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20150423102912) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
