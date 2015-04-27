dfb5bc2654eb723712a27bfd4ed3c51d94a88ec5
toyama satoshi <toyamarinyon@gmail.com>
2015-04-27 11:52:26 +0900

    Section 11.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find params[:id]
+    @micropost = @user.microposts.paginate page: params[:page]
   end
 
   def create
