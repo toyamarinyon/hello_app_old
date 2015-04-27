28b0d31f2a673ee21a787477e80e69d4cac0893c
toyama satoshi <toyamarinyon@gmail.com>
2015-04-27 17:50:47 +0900

    Section 12.3.1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -84,4 +84,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
   end
 
+  test "feed should have the right posts" do
+    michael = users :michael
+    archer  = users :archer
+    lana    = users :lana
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include? post_following
+    end
+    # Posts from self
+    micael.microposts.each do |self_following|
+      assert michael.feed.include? self_following
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include? post_unfollowed
+    end
+  end
+
 end
