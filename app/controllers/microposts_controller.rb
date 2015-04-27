350fb2240d37d779e16811897910f2593d0db351
toyama satoshi <toyamarinyon@gmail.com>
2015-04-27 12:35:40 +0900

    Section 11.3.3

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,6 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Microposts created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
